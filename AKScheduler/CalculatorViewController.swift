e55fe7b8673ad35390a40cb53d2c0b7cee6052f1
濱口和希 <>
2018-04-02 20:17:00 +0900

    add Calculator,Setting,Schedule_view

---
+++ AKScheduler/CalculatorViewController.swift
@@ -0,0 +1,47 @@
+//
+//  CalculatorViewController.swift
+//  AKScheduler
+//
+//  Created by 濱口和希 on 2018/04/02.
+//  Copyright © 2018年 KagatukiMizuha. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupGestureRecognizer()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    ///スワイプ処理の初期化
+    func setupGestureRecognizer() {
+        //スワイプを判断するための変数を定義
+        let leftSwipeRecognizer  = UISwipeGestureRecognizer(target: self, action: #selector(leftSwiped(_:)))
+        //スワイプの方向を判断
+        leftSwipeRecognizer.direction  = .left
+        //viewに追加
+        self.view.addGestureRecognizer(leftSwipeRecognizer)
+    }
+    
+    @objc func leftSwiped(_ sender: Any) {
+        print("back main")
+        dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
