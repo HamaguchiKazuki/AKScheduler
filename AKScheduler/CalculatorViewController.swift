ddaaff4856c019de4d9ff229dcad4633ea149f42
濱口和希 <>
2018-04-08 19:50:55 +0900

    電卓機能を追加

--- AKScheduler/CalculatorViewController.swift
+++ AKScheduler/CalculatorViewController.swift
@@ -48,8 +48,41 @@ class CalculatorViewController: UIViewController {
         guard let formulaText = formulaLabel.text else {
             return
         }
-        let fotmula: String = formatFormula()
-        answerLanel.text = evalFormula()
+        let formula: String = formatFormula(formulaText)
+        answerLabel.text = evalFormula(formula)
     }
     
+    private func formatFormula(_ formula: String) -> String {
+        //入力された整数には’.0’を追加して小数として扱う
+        //また'÷'を'/'に、'x'を'*'に置換する
+        let formattedFormula: String = formula.replacingOccurrences(
+            of: "(?<=^|[÷×\\+\\-\\(])([0-9]+)(?=[÷×\\+\\-\\)]|$)",
+            with: "$1.0",
+            options: NSString.CompareOptions.regularExpression,
+            range: nil
+        ).replacingOccurrences(of: "÷", with: "/").replacingOccurrences(of: "×", with: "*")
+        return formattedFormula
+    }
+    
+    private func evalFormula(_ formula: String) -> String {
+        do {
+            //Expressionで文字列の計算式を評価して答えを求める
+            let expression = Expression(formula)
+            let answer = try expression.evaluate()
+            return formatAnswer(String(answer))
+        } catch {
+            //計算式が不当だった場合
+            return "式を正しく入力してください"
+        }
+    }
+    
+    private func formatAnswer(_ answer: String) -> String {
+        //答えの小数点以下が'.0'だった場合は'.0'を削除して答えを整数で表示する
+        let formattedAnswer: String = answer.replacingOccurrences(
+            of: "//.0$",
+            with: "",
+            options: NSString.CompareOptions.regularExpression,
+            range: nil)
+        return formattedAnswer
+    }
 }
